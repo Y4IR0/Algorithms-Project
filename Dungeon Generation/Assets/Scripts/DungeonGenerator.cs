a715e38ac17c3e61c57384760d17e02f35192e87
Y4IR0 <yairo_k@proton.me>
2025-03-17 13:26:01 +0100

    working on minroom size

--- Dungeon Generation/Assets/Scripts/DungeonGenerator.cs
+++ Dungeon Generation/Assets/Scripts/DungeonGenerator.cs
@@ -6,43 +6,100 @@ using NaughtyAttributes;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    RectInt boundary = new RectInt(0, 0, 40, 40);
     new List<RectInt> rooms = new List<RectInt>();
-    private int splitAmount = 4;
-    
-    [SerializeField] RectInt maxRoomRect = new RectInt(0, 0, 15, 15);
+    new List<RectInt> horizontalSplittableRooms = new List<RectInt>();
+    new List<RectInt> verticalSplittableRooms = new List<RectInt>();
+ 
+    [SerializeField] RectInt boundary = new RectInt(0, 0, 200, 200);
+    [SerializeField] RectInt minRoomRect = new RectInt(0, 0, 10, 10);
     //[SerializeField] int wallThickness = 2;
 
 
 
     void DrawRooms()
     {
-        AlgorithmsUtils.DebugRectInt(boundary, Color.green, 100000, false, .1f);
-        
-        foreach (RectInt room in rooms)
+        AlgorithmsUtils.DebugRectInt(boundary, Color.green, 100000, false, 0);
+
+        for (int i = 0; i < rooms.Count; i++)
         {
-            AlgorithmsUtils.DebugRectInt(room, Color.yellow, 100000, false, .01f);
+            RectInt room = rooms[i];
+            AlgorithmsUtils.DebugRectInt(room, Color.yellow, 100000, false, .03f * i);
         }
     }
 
     void SplitRoom(RectInt room, bool splitHorizontally)
     {
+        RectInt room1;
+        RectInt room2;
+
+        float room1Divider = Random.Range(3, 7) * .1f;
+        float room2Divider = 1 - room1Divider;
+
+        Debug.Log(room1Divider);
+        Debug.Log(room2Divider);
+        
         if (splitHorizontally)
         {
-            RectInt room1 = new RectInt(room.x, room.y, room.width/2, room.height);
-            RectInt room2 = new RectInt(room.x + room.width/2, room.y, room.width/2, room.height);
+            int room1Height;
+            int room2Height;
             
-            rooms.Add(room1);
-            rooms.Add(room2);
+            if (room.height % 2 == 0)   // Even
+            {
+                room1Height = Mathf.RoundToInt(room.height * room1Divider);
+                room2Height = Mathf.RoundToInt(room.height * room2Divider);
+            }
+            else                        // Odd
+            {
+                room1Height = Mathf.RoundToInt(room.height * room1Divider);
+                room2Height = Mathf.RoundToInt(room.height * room2Divider + .49999f);
+            }
+            
+            room1 = new RectInt(room.x, room.y + room2Height, room.width, room1Height);
+            room2 = new RectInt(room.x, room.y, room.width, room2Height);
         }
         else
         {
-            RectInt room1 = new RectInt(room.x, room.y + room.height/2, room.width, room.height/2);
-            RectInt room2 = new RectInt(room.x, room.y, room.width, room.height/2);
+            int room1Width;
+            int room2Width;
             
-            rooms.Add(room1);
-            rooms.Add(room2);
+            if (room.width % 2 == 0)    // Even
+            {
+                room1Width = Mathf.RoundToInt(room.width * room1Divider);
+                room2Width = Mathf.RoundToInt(room.width * room2Divider);
+            }
+            else                        // Odd
+            {
+                room1Width = Mathf.RoundToInt(room.width * room1Divider);
+                room2Width = Mathf.RoundToInt(room.width * room2Divider + .49999f);
+            }
+            
+            room1 = new RectInt(room.x, room.y, room1Width, room.height);
+            room2 = new RectInt(room.x + room1Width, room.y, room2Width, room.height);
         }
+        
+        
+        
+        
+        // Check if horizontally splittable
+        Debug.Log(room1);
+        if (room1.width > minRoomRect.width && room2.width > minRoomRect.width)
+        {
+            horizontalSplittableRooms.Add(room1);
+            horizontalSplittableRooms.Add(room2);
+        }
+            
+        // Check if vertically splittable
+        if (room1.height > minRoomRect.height && room2.height > minRoomRect.height)
+        {
+            verticalSplittableRooms.Add(room1);
+            verticalSplittableRooms.Add(room2);
+        }
+        
+        rooms.Add(room1);
+        rooms.Add(room2);
+        
+        horizontalSplittableRooms.Remove(room);
+        //verticalSplittableRooms.Remove(room);
 
         rooms.Remove(room);
     }
@@ -55,12 +112,23 @@ public class DungeonGenerator : MonoBehaviour
     [Button("Generate Rooms")]
     void GenerateRooms()
     {
-        SplitRoom(boundary, Random.Range(0, 2) == 0);
+        SplitRoom(boundary, Random.Range(0, 1) == 1);
 
-        for (int i = 0; i < splitAmount; i++)
+        int amount = 0;
+        while (horizontalSplittableRooms.Count > 0 && amount < 100)
         {
-            SplitRoom(rooms[Random.Range(0, rooms.Count)], Random.Range(0, 2) == 0);
+            amount++;
+            SplitRoom(horizontalSplittableRooms[0], Random.Range(0, 1) == 1);
         }
+        
+        
+        int amount2 = 0;
+        while (verticalSplittableRooms.Count > 0 && amount2 < 100)
+        {
+            amount2++;
+            SplitRoom(verticalSplittableRooms[0], Random.Range(0, 1) == 1);
+        }
+        
 
         DrawRooms();
     }
